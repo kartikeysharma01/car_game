deffa5ae1c47ae7648f5acec466d69151d5a5ad9
Kartikey Sharma <168752408+kartikeysharma01@users.noreply.github.com>
2024-11-22 11:16:59 +0530

    Add files via upload

---
+++ AppLauncher.java
@@ -0,0 +1,8 @@
+package com.game;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+        AppFrame aFrame = new AppFrame();
+    }
+}
